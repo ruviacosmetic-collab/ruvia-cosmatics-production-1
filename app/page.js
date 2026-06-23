17ff43e8253f013ee4a7d23e79efb179f6af683e
ruviacosmetic-collab <your-github-ruviacosmetic@gmail.com>
2026-06-23 13:04:38 +0530

    Your commit message

--- app/page.js
+++ app/page.js
@@ -589,7 +589,7 @@ export default function Home() {
               evenly when the source ratio isn't an exact match.
             */}
             <img
-              src="/images/founder1.jpeg``"
+              src="/images/founder.jpeg``"
               alt="Dr. Chitra Bhati, Founder of Ruvia Cosmetics"
               className="w-full h-full object-cover object-center"
             />
