303eb6e7691b17dbfc5cf06c6b0dddda107f72fc
ruviacosmetic-collab <your-github-ruviacosmetic@gmail.com>
2026-07-22 22:03:44 +0530

    Founder image update

--- app/page.js
+++ app/page.js
@@ -589,7 +589,7 @@ export default function Home() {
               evenly when the source ratio isn't an exact match.
             */}
             <img
-              src="/images/founder.jpg"
+              src="/images/founder.png"
               alt="Dr. Chitra Bhati, Founder of Ruvia Cosmetics"
               className="w-full h-full object-cover object-center"
             />
